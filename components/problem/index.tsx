6ebad51740a45abecfbae53ec509c9a295ee0b8b
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-25 15:13:54 -0300

    refactor: remove second list

--- components/problem/index.tsx
+++ components/problem/index.tsx
@@ -5,30 +5,16 @@ export function Problem() {
         Se sente desorganizado na hora do treino?
       </h2>
 
-      <ul className="flex flex-col gap-2 text-muted-foreground mt-4 text-sm px-2 list-disc pl-8">
+      <ul className="flex flex-col gap-2 text-muted-foreground mt-4 mb-16 text-sm px-2 list-disc pl-8">
         <li>
           Treinar sem organização faz você{" "}
           <span className="underline">perder</span> consistência.
         </li>
-        <li>Vídeos espalhados, anotações soltas, nada conectado.</li>
+        <li>Vídeos espalhados, anotações soltas e nada centralizado.</li>
+        <li>É fácil esquecer cargas, repetições e progresso.</li>
+        <li>Cronometrar descanso no celular vira distração.</li>
+        <li>Sem histórico, fica difícil saber se você realmente evoluiu.</li>
       </ul>
-
-      <div className="mt-8">
-        <h3 className="text-center text-2xl">
-          Tudo o que você precisa, de graça.
-        </h3>
-
-        <ul className="flex flex-col gap-2 text-muted-foreground mt-4 text-sm px-2 list-disc pl-8">
-          <li>Salve links de vídeos para assistir durante o treino.</li>
-          <li>Inicie e salve o cronômetro de cada sessão.</li>
-          <li>Faça anotações e guarde no seu bloco de notas.</li>
-          <li>Tenha um histórico detalhado das suas sessões.</li>
-        </ul>
-
-        <span className="text-center block my-4 text-muted-foreground">
-          Tudo em um só lugar.
-        </span>
-      </div>
     </section>
   );
 }
