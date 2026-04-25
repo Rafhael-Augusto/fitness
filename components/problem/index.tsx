c80d68e2c5ace4f62a6c267ca60ace7954c6b28f
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-25 16:31:40 -0300

    style: add underline to words

--- components/problem/index.tsx
+++ components/problem/index.tsx
@@ -8,12 +8,24 @@ export function Problem() {
       <ul className="flex flex-col gap-2 text-muted-foreground mt-4 mb-16 text-sm px-2 list-disc pl-8">
         <li>
           Treinar sem organização faz você{" "}
-          <span className="underline">perder</span> consistência.
+          <span className="underline">perder consistência</span> .
         </li>
-        <li>Vídeos espalhados, anotações soltas e nada centralizado.</li>
-        <li>É fácil esquecer cargas, repetições e progresso.</li>
-        <li>Cronometrar descanso no celular vira distração.</li>
-        <li>Sem histórico, fica difícil saber se você realmente evoluiu.</li>
+        <li>
+          Vídeos espalhados, anotações soltas e{" "}
+          <span className="underline">nada centralizado</span>.
+        </li>
+        <li>
+          É <span className="underline">fácil esquecer</span> cargas, repetições
+          e progresso.
+        </li>
+        <li>
+          Cronometrar descanso no celular vira{" "}
+          <span className="underline">distração</span>.
+        </li>
+        <li>
+          Sem histórico, fica difícil saber se você{" "}
+          <span className="underline">realmente evoluiu</span>.
+        </li>
       </ul>
     </section>
   );
