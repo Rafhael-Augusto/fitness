d7abc439c93a9098590288ec54e64771aaccc58b
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-25 17:35:49 -0300

    style: remove button tag

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -7,12 +7,14 @@ import { Button } from "@/components/ui/button";
 export function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4">
-      <button>
-        <Link href={"/"} className="flex items-center gap-2">
-          <DumbbellIcon size={"25"} />
-          <span className="text-xl font-bold hidden md:inline">Fitness</span>
-        </Link>
-      </button>
+      <Link
+        href={"/"}
+        className="flex items-center gap-2"
+        aria-label="Home page button"
+      >
+        <DumbbellIcon size={"25"} />
+        <span className="text-xl font-bold hidden md:inline">Fitness</span>
+      </Link>
 
       <div>
         <Button>Comece Agora</Button>
