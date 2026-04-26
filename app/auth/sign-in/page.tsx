1d726d992355d60cf1733f8ffff54e14bc9752f0
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-25 23:30:19 -0300

    chore: reorganize component imports

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,9 +1,10 @@
-import { SignInForm } from "@/components/auth/sign-in";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { SignInForm } from "@/components/auth/sign-in";
 
 export default function SignInPage() {
   return (
-    <section className="items-center px-4 mt-24">
+    <section className="items-center px-4 mt-24 h-screen">
       <Card className="">
         <CardHeader>
           <CardTitle>Fazer login</CardTitle>
