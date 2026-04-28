44251392a273c6e078ed85e3b1352cd471286650
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-28 16:07:04 -0300

    style: add margin top and bottom

--- components/start-session/stopwatch/index.tsx
+++ components/start-session/stopwatch/index.tsx
@@ -2,8 +2,8 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export function Stopwatch() {
   return (
-    <Card>
-      <CardContent className="flex justify-center">
+    <Card className="mt-8">
+      <CardContent className="flex justify-center mb-32">
         <span className="text-2xl font-bold">00:00</span>
       </CardContent>
     </Card>
