fc4cb3d35ce9500bc4201c58fe0dacb066152e92
Rafhael <rafhaelaugustodev@gmail.com>
2026-05-02 15:45:33 -0300

    fix: add mock data to GoalsSelected component

--- app/session/page.tsx
+++ app/session/page.tsx
@@ -6,6 +6,33 @@ import {
   YoutubePlayer,
 } from "@/features/session/start";
 
+const goalSelected = {
+  id: "123213213213",
+  title: "Primeira meta legal",
+  inputs: [
+    {
+      id: "819374t231870",
+      label: "Cronometro",
+      value: 200,
+    },
+    {
+      id: "819374t2231870",
+      label: "Agachamentos",
+      value: 200,
+    },
+    {
+      id: "8193724t231870",
+      label: "Porra",
+      value: 200,
+    },
+    {
+      id: "81937224t231870",
+      label: "Porra",
+      value: 200,
+    },
+  ],
+};
+
 export default function StartSessionPage() {
   return (
     <>
@@ -15,7 +42,7 @@ export default function StartSessionPage() {
 
       <NotepadSheet />
 
-      <GoalSelected />
+      <GoalSelected data={goalSelected} />
 
       <Stopwatch />
     </>
