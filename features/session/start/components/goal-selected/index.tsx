46f220e7f356f2891df4b4205c93a2b321002c73
Rafhael <rafhaelaugustodev@gmail.com>
2026-05-02 14:16:41 -0300

    feat: add checkbox

--- features/session/start/components/goal-selected/index.tsx
+++ features/session/start/components/goal-selected/index.tsx
@@ -1,51 +1,81 @@
+"use client";
+
+import { useState } from "react";
+
+import { Checkbox } from "@/shared/components/checkbox";
 import { Card, CardContent } from "@/shared/components/card";
 
-const goalSelected = {
-  id: "123213213213",
-  title: "Primeira meta legal",
-  inputs: [
-    {
-      id: "819374t231870",
-      label: "Cronometro",
-      value: 200,
-    },
-    {
-      id: "819374t2231870",
-      label: "Agachamentos",
-      value: 200,
-    },
-    {
-      id: "8193724t231870",
-      label: "Porra",
-      value: 200,
-    },
-    {
-      id: "81937224t231870",
-      label: "Porra",
-      value: 200,
-    },
-  ],
+type Inputs = {
+  id: string;
+  label: string;
+  value: string | number;
 };
 
-export function GoalSelected() {
+type Props = {
+  data: {
+    id: string;
+    title: string;
+    inputs: Inputs[];
+  };
+};
+
+export function GoalSelected({ data }: Props) {
+  const [goalAchieved, setGoalAchieved] = useState(
+    data.inputs.reduce(
+      (acc, item) => {
+        acc[item.id] = false;
+
+        return acc;
+      },
+      {} as Record<string, boolean>,
+    ),
+  );
+
+  const handleGoalChange = (id: string) => {
+    const inputValue = goalAchieved[id];
+
+    setGoalAchieved((prev) => ({
+      ...prev,
+      [id]: !inputValue,
+    }));
+  };
+
+  const values = Object.values(goalAchieved);
+
+  const total = values.length;
+  const completed = values.filter(Boolean).length;
+
+  const percentage = total ? (completed / total) * 100 : 0;
+
   return (
     <>
       <Card>
         <CardContent>
-          <span className="text-center text-xl block mb-2">
-            {goalSelected.title}
-          </span>
+          <span className="text-center text-xl block mb-2">{data.title}</span>
 
           <Card>
             <CardContent className="flex flex-col gap-4">
-              {goalSelected.inputs.map((item) => (
+              {data.inputs.map((item) => (
                 <div key={item.id} className="flex flex-col">
-                  <span className="text-lg">{item.label}</span>
+                  <div
+                    onClick={() => handleGoalChange(item.id)}
+                    className="flex items-center justify-between"
+                  >
+                    <span className="text-lg">{item.label}</span>
+                    <Checkbox
+                      checked={goalAchieved[item.id]}
+                      className="border-muted-foreground"
+                    />
+                  </div>
                   <span className="text-muted-foreground bg-muted p-2 rounded-xl">
                     {item.value}
                   </span>
                 </div>
               ))}
+
+              <div className="text-center text-lg">
+                <span>{percentage}%</span>
+              </div>
             </CardContent>
           </Card>
         </CardContent>
